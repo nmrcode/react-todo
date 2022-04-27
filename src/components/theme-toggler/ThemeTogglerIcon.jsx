5109e1fa8204468db52f8b706d498c6851519769
nmcode <nmcode.personal@gmail.com>
2022-04-28 02:02:35 +0300

    clear readme and fix toggler

--- src/components/theme-toggler/ThemeTogglerIcon.jsx
+++ src/components/theme-toggler/ThemeTogglerIcon.jsx
@@ -1,14 +1,28 @@
 import styled from "styled-components";
 
 export const ThemeTogglerIcon = styled.div`
+  cursor: pointer;
   display: flex;
   align-items: center;
   justify-content: center;
   position: fixed;
-  bottom: 10px;
-  right: 10px;
-  width: 40px;
-  height: 40px;
+  bottom: 30px;
+  right: 30px;
+  width: 80px;
+  height: 80px;
   box-shadow: ${({ theme }) => theme.shadowItem};
   border-radius: 50%;
+  background-color: ${({ theme }) => theme.body};
+
+  svg {
+    font-size: 30px;
+  }
+
+  @media (max-width: 768px) {
+    width: 60px;
+    height: 60px;
+    svg {
+      font-size: 25px;
+    }
+  }
 `;
